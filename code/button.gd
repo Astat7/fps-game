c10dc2ab2f5bc960f970d22a795c114dc0f6c430
Radislav Střílek <96394461+Astat7@users.noreply.github.com>
2025-04-02 18:48:57 +0200

    Update button.gd

--- code/button.gd
+++ code/button.gd
@@ -3,16 +3,6 @@ extends Node3D
 var locked = false
 var used = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-
 func _on_interacted_signal() -> void:
 	if locked == true:
 		return
